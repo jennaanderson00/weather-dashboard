7d5ba8841abe2e8731ff2c6af9eb02e1606c7b75
0ffpudding <jennaanderson00@gmail.com>
2022-04-08 20:18:46 -0500

    added criteria guidelines to JS

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+/* CRITERIA
+display current and future weather conditions
+    city name
+    date
+    weather conditions
+    temperature
+    humidity
+    wind speed
+    uv index
+cities are searchable
+searched cities are added to search history and can be chosen from there
+uv index color codes conditions 
+    favorable
+    moderate
+    severe
+future conditions include a 5-day forecast with aforementioned information
+*/
